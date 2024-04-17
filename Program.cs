b177d49d9844fde88c30f81bf9a87d7fc5ce646a
Tamerlan <tamerlan.recebov062@gmail.com>
2024-04-17 04:12:20 +0400

    Dobavili massiv iz strok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreatString(string[] strings)
+{
+    int count = 0;
+
+    foreach (string str in strings)
+    {
+        if (str.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] array_str = new string[count];
+    int index = 0;
+
+    foreach (string str in strings)
+    {
+        if (str.Length <= 3)
+        {
+            array_str[index] = str;
+            index++;
+        }
+    }
+    return array_str;
+}
